9002f724de7b5cbe48eb11380a94bcad3e61aec5
tobeh <66030584+toobeeh@users.noreply.github.com>
2025-11-28 01:47:56 +0100

    #78 init breadcrumbs feature

---
+++ src/app/breadcrumbs.ts
@@ -0,0 +1,30 @@
+export class Breadcrumb {
+  private _id;
+
+  public get parent() { return this._parent; }
+  public get name() { return this._name; }
+  public get href() { return this._href; }
+  public get id() { return this._id; }
+
+  constructor(private _name: string, private readonly _href: string, private readonly _parent?: Breadcrumb) {
+    this._id = Symbol(_name);
+  }
+
+  public set name(name: string) {
+    this._name = name;
+  }
+
+  /**
+   * Clone this breadcrumb while preserving the id
+   * @private
+   */
+  public clone(): Breadcrumb {
+    const clone = new Breadcrumb(this._name, this._href, this._parent ? this._parent.clone() : undefined);
+    clone._id = this._id;
+    return clone;
+  }
+}
+
+
+export const BC_EVENTS = new Breadcrumb("Events", "events");
+export const BC_SHIFTS = new Breadcrumb("Shifts", "shifts", BC_EVENTS);
---
+++ src/app/guards/breadcrumbs/breadcrumbs.guard.ts
@@ -0,0 +1,26 @@
+import { CanActivateFn } from '@angular/router';
+import {Breadcrumb} from '../../breadcrumbs';
+import {inject} from '@angular/core';
+import {PageService} from '../../services/breadcrumbs/page.service';
+
+export const breadcrumbsGuard: CanActivateFn = (route, state) => {
+
+  /* set breadcrumbs only for leaf routes */
+  if(route.children.length > 0) return true;
+
+  const pageService = inject(PageService);
+
+  /* get breadcrumbs of route data */
+  const routeData = route.data;
+  const breadcrumbs = routeData['breadcrumbs'];
+
+  /* if found, set as current breadcrumbs */
+  if(breadcrumbs instanceof Breadcrumb) {
+    pageService.withBreadcrumbs(breadcrumbs);
+  }
+  else {
+    pageService.withBreadcrumbs(undefined);
+  }
+
+  return true;
+};
