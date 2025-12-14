837098b5a63ef4581781a2ceb44d7918662d280d
tobeh <66030584+toobeeh@users.noreply.github.com>
2025-12-14 18:03:23 +0100

    #113 Fix FE linting

--- src/app/components/tooltip/tooltip.component.spec.ts
+++ src/app/components/tooltip/tooltip.component.spec.ts
@@ -1,8 +1,8 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { TooltipComponent } from './tooltip.component';
+import { TooltipComponent } from "./tooltip.component";
 
-describe('TooltipComponent', () => {
+describe("TooltipComponent", () => {
   let component: TooltipComponent;
   let fixture: ComponentFixture<TooltipComponent>;
 
@@ -17,7 +17,7 @@ describe('TooltipComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
