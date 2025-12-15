1dba122e5167f248b9183a1ffbd84558e256d45b
tobeh <66030584+toobeeh@users.noreply.github.com>
2025-12-15 18:53:22 +0100

    #113 Fix lint and conditions

--- src/app/pages/shiftcontrol/plan-onboarding/plan-onboarding.component.ts
+++ src/app/pages/shiftcontrol/plan-onboarding/plan-onboarding.component.ts
@@ -3,12 +3,15 @@ import {PageService} from "../../../services/page/page.service";
 import {BC_PLAN_DASHBOARD, BC_EVENT} from "../../../breadcrumbs";
 import {InputButtonComponent} from "../../../components/inputs/input-button/input-button.component";
 import {RouterLink} from "@angular/router";
+import {FaIconComponent} from "@fortawesome/angular-fontawesome";
+import {faCalendar} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: "app-plan-onboarding",
   imports: [
     InputButtonComponent,
-    RouterLink
+    RouterLink,
+    FaIconComponent
   ],
   standalone: true,
   templateUrl: "./plan-onboarding.component.html",
@@ -16,6 +19,7 @@ import {RouterLink} from "@angular/router";
 })
 export class PlanOnboardingComponent {
 
+  protected readonly iconDate = faCalendar;
   private readonly _pageService = inject(PageService);
 
   constructor() {
@@ -23,5 +27,4 @@ export class PlanOnboardingComponent {
       .configureBreadcrumb(BC_EVENT, "Pilot Event", "eventId")
       .configureBreadcrumb(BC_PLAN_DASHBOARD, "Pilot Plan", "planId");
   }
-
 }
