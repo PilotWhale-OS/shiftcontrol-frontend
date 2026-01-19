34a9c2ca63959f69db2191ad609461de847800aa
tobeh <66030584+toobeeh@users.noreply.github.com>
2026-01-19 02:16:39 +0100

    #175 Integrate active markers

--- src/app/pages/shiftcontrol/home/home.component.ts
+++ src/app/pages/shiftcontrol/home/home.component.ts
@@ -2,8 +2,9 @@ import {Component, inject} from "@angular/core";
 import {RouterLink} from "@angular/router";
 import {UserService} from "../../../services/user/user.service";
 import {AsyncPipe, NgClass} from "@angular/common";
-import {map} from "rxjs";
+import {BehaviorSubject, map, withLatestFrom} from "rxjs";
 import {icons} from "../../../util/icons";
+import {EventEndpointService} from "../../../../shiftservice-client";
 
 @Component({
   selector: "app-home",
@@ -17,13 +18,30 @@ import {icons} from "../../../util/icons";
 })
 export class HomeComponent {
 
-  protected readonly cards = [
-    {title:"Events", content: "Browse all your events", href: "events"},
-    {title:"Pilot Plan", content: "Pilot Event: Currently active\n(coming soon)", href: "/", spotlight: true}
-  ];
+  protected readonly cards$ = new BehaviorSubject([
+    {title:"User Account", content: "View your account and set preferences", href: "me", spotlight: false},
+    {title:"Events", content: "Browse all your events", href: "events", spotlight: false},
+  ]);
   protected readonly icons = icons;
 
   private readonly _userService = inject(UserService);
+  private readonly _eventService = inject(EventEndpointService);
+
+  constructor() {
+    this._eventService.getAllOngoingEvents().pipe(
+      withLatestFrom(this.cards$)
+    ).subscribe(([events, cards]) => {
+      this.cards$.next([
+        ...cards,
+        ...events.map(event => ({
+          title: event.name,
+          content: event.shortDescription ?? `${event.name} is currently ongoing. Click to view details.`,
+          href: `events/${event.id}`,
+          spotlight: true
+        }))
+      ]);
+    });
+  }
 
   protected get name$(){
     return this._userService.kcProfile$.pipe(
