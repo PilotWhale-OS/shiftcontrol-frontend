eb08eac3450e08d98873694a36d1d4fccea1d3a7
tobeh <66030584+toobeeh@users.noreply.github.com>
2026-01-22 23:01:46 +0100

    #279 Add username filter

--- src/app/pages/shiftcontrol/application-users/application-users.component.ts
+++ src/app/pages/shiftcontrol/application-users/application-users.component.ts
@@ -1,11 +1,13 @@
 import {Component, inject} from "@angular/core";
 import { icons } from "../../../util/icons";
-import {FormBuilder} from "@angular/forms";
+import {FormBuilder, ReactiveFormsModule} from "@angular/forms";
 import {TrustAlertDisplayDto, TrustAlertEndpointService, UserEventEndpointService} from "../../../../shiftservice-client";
 import {debounceTime, shareReplay, startWith, switchMap} from "rxjs";
 import {AsyncPipe, DatePipe} from "@angular/common";
 import {InputButtonComponent} from "../../../components/inputs/input-button/input-button.component";
 import {RouterLink} from "@angular/router";
+import {InputTextComponent} from "../../../components/inputs/input-text/input-text.component";
+import {TypedFormControlDirective} from "../../../directives/typed-form-control.directive";
 
 @Component({
   selector: "app-application-users",
@@ -14,7 +16,10 @@ import {RouterLink} from "@angular/router";
     AsyncPipe,
     DatePipe,
     InputButtonComponent,
-    RouterLink
+    RouterLink,
+    InputTextComponent,
+    ReactiveFormsModule,
+    TypedFormControlDirective
   ],
   templateUrl: "./application-users.component.html",
   styleUrl: "./application-users.component.scss"
@@ -32,14 +37,15 @@ export class ApplicationUsersComponent {
 
   constructor() {
     this.form = this._fb.group({
-      paginationIndex: this._fb.nonNullable.control<number>(0)
+      paginationIndex: this._fb.nonNullable.control<number>(0),
+      search: this._fb.nonNullable.control<string>("")
     });
 
     this.page$ = this.form.valueChanges.pipe(
       startWith(this.form.value),
       debounceTime(100),
       switchMap((value) => this._userEventService.getAllUsers(value.paginationIndex ?? 0, this.pageSize, {
-        name: undefined
+        name: value.search
       })),
       shareReplay()
     );
