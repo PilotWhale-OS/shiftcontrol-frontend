c1604f6d0aad3ecf1a815be3a2fbb9e694750e19
tobeh <66030584+toobeeh@users.noreply.github.com>
2026-01-26 20:20:31 +0100

    #344 Add reward points redeem url

--- src/app/components/event-rewards/event-rewards.component.ts
+++ src/app/components/event-rewards/event-rewards.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input } from "@angular/core";
+import {Component, inject, Input} from "@angular/core";
 import {EventShiftPlansOverviewDto} from "../../../shiftservice-client";
 import {BehaviorSubject} from "rxjs";
 import {FaIconComponent} from "@fortawesome/angular-fontawesome";
 import {icons} from "../../util/icons";
 import {AsyncPipe} from "@angular/common";
 import {InputButtonComponent} from "../inputs/input-button/input-button.component";
+import {ToastService} from "../../services/toast/toast.service";
 
 @Component({
   selector: "app-event-rewards",
@@ -21,8 +22,18 @@ export class EventRewardsComponent {
   protected readonly icons = icons;
   protected eventDashboard$ = new BehaviorSubject<undefined | EventShiftPlansOverviewDto>(undefined);
 
+  private readonly _toastService = inject(ToastService);
+
   @Input()
   public set eventDashboard(value: EventShiftPlansOverviewDto) {
     this.eventDashboard$.next(value);
+  }
+
+  protected goToRewards(event: EventShiftPlansOverviewDto){
+    if(event.eventOverview.rewardPointsRedeemUrl !== undefined) {
+      window.open(event.eventOverview.rewardPointsRedeemUrl, "_blank");
+    } else {
+      this._toastService.showInfo("Stay tuned..", "There are no rewards yet. Event organizers will add them when they're ready!");
+    }
   }
 }
